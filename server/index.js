938446d0cdb59c966827667203a53059a3cbafd8
Sam Pepose <sampepose@gmail.com>
2016-09-25 23:54:11 -0500

    fixes to package.json and removed es6 from server

--- server/index.js
+++ server/index.js
@@ -7,26 +7,26 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: true }
 }));
-
-app.post('/api/login', (req, res) => {
+app.use(express.static('../src'));
+app.post('/api/login', function(req, res) {
   if (!req.body.username || !req.body.password) {
     res.status(404).send('Invalid payload');
   }
   // TODO: Login
 });
 
-app.get('/api/image/', (req, res) => {
+app.get('/api/image/', function(req, res) {
   if (!req.session.username) {
     res.status(404).send('Not logged in');
   }
 });
 
-app.post('/api/label/', (req, res) => {
+app.post('/api/label/', function(req, res) {
   if (!req.session.username) {
     res.status(404).send('Not logged in');
   }
 });
 
-app.listen(3000, function () {
+app.listen(8001, function () {
   console.log('Example app listening on port 3000!');
 });
