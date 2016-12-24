bbcd72feb1601ef47249e06142c0c4c5a55a7216
Sam Pepose <sampepose@gmail.com>
2016-12-23 20:43:19 -0600

    Added signup/login to API

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var session = require('express-session');
+var bodyParser = require('body-parser')
+var {Users} = require('./models');
 
 var app = express();
-
+app.use(bodyParser.json({}));
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -16,10 +18,54 @@ if (process.argv[2] === 'prod') {
 }
 
 app.post('/api/login', function(req, res) {
-  if (!req.body.username || !req.body.password) {
+  if (!req.body || !req.body.username || !req.body.password) {
     res.status(404).send('Invalid payload');
+    return;
   }
-  // TODO: Login
+
+  Users.findOne({username: req.body.username}, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send({ error: 'Error signing in user' });
+    } else {
+      if (user) {
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (err) {
+            console.error(err);
+            res.status(400).send({ error: 'Error signing in user' });
+          } else if (!isMatch) {
+            res.status(401).send({ error: 'Error authorizing user' });
+          } else {
+            req.session.user_id = user._id;
+
+            res.status(201).send();
+          }
+        });
+      } else {
+        res.status(401).send({ error: 'Error authorizing user' });
+      }
+    }
+  });
+});
+
+app.post('/api/signup', function(req, res) {
+  if (!req.body || !req.body.username || !req.body.password) {
+    res.status(404).send('Invalid payload');
+    return;
+  }
+
+  // Create the User in the database
+  new Users(req.body).save((err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send({ error: 'Error creating user' });
+    } else {
+      // Add the proper session data
+      req.session.user_id = user._id;
+
+      res.status(201).send();
+    }
+  });
 });
 
 app.get('/api/image/', function(req, res) {
--- server/models.js
+++ server/models.js
@@ -1,9 +1,20 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
-const MONGO_URL = require('./config');
-const Schema = {mongoose};
+const {MONGO_URL} = require('./config');
+const {Schema} = mongoose;
+const {ObjectId} = mongoose.Schema.Types;
+const NUM_SALT_ROUNDS = 10; // really means 2^10 rounds
 
-mongoose.connect(MONGO_URL);
+// Attempt to connect to MongoDB server
+mongoose.connect(MONGO_URL, err => {
+  if (err) {
+    console.error("ERROR: Could not connect to the mongo db. Is `mongod` running?");
+    process.exit();
+  }
+});
 
 const UserSchema = new Schema({
   username: {
@@ -14,8 +25,19 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true,
+    set: function(password) {
+      return bcrypt.hashSync(password, NUM_SALT_ROUNDS);
+    },
   },
 });
+
+// http://stackoverflow.com/questions/14588032/mongoose-password-hashing
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
 
 const ImageSchema = new Schema({
   uri: {
